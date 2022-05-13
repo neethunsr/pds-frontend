7b5b9be2f6558056b670d8cdd5d28dc78278857d
Neethu S <neethu2001nsr@gmail.com>
2022-05-13 12:07:32 +0530

    Added demo signin page

---
+++ src/App.js
@@ -0,0 +1,15 @@
+// import logo from './logo.svg';
+import "./App.css";
+// import Login from "./Login";
+import SignIn from "./SignIn";
+
+function App() {
+	return (
+		<div className="App">
+			{/* <Login /> */}
+			<SignIn />
+		</div>
+	);
+}
+
+export default App;
