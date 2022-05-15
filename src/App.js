0b463b9756653da266c9f039ed44c17900ceff61
Neethu S <neethu2001nsr@gmail.com>
2022-05-15 15:14:23 +0530

    Pushing from dell

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 // import logo from './logo.svg';
 import "./App.css";
+import Footer from "./components/Footer";
 // import Login from "./Login";
-import SignIn from "./SignIn";
+// import SignIn from "./SignIn";
+// import SignInSide from "./SignInSide";
 
 function App() {
 	return (
 		<div className="App">
 			{/* <Login /> */}
-			<SignIn />
+			<Footer />
 		</div>
 	);
 }
