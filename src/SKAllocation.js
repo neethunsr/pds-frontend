54150a870a97363ddc5b2fd02815dae62fb212fe
Neethu S <neethu2001nsr@gmail.com>
2022-05-26 00:37:01 +0530

    Shopkeeper pg

--- src/SKAllocation.js
+++ src/SKAllocation.js
@@ -1,88 +1,159 @@
-import * as React from 'react';
-import { TextareaAutosize,Box,Container,TextField,Button } from '@mui/material';
-import { useForm } from 'react-hook-form';
-
+import * as React from "react";
+import {
+  TextareaAutosize,
+  Box,
+  Container,
+  TextField,
+  Button,
+  InputAdornment,
+} from "@mui/material";
+import { useForm } from "react-hook-form";
 
 const IVAllocation = () => {
+  const { register, handleSubmit } = useForm();
 
-    const {register,handleSubmit} = useForm()
+  const onSubmit = (data) => {
+    console.log(data);
+  };
 
-    const onSubmit = (data) =>{
-        console.log(data)
-    }
-
-    return (
-        <div className = "mt-5">
-            <h1>CUSTOMER ALLOCATION</h1>
-        <Container 
-      maxWidth = "xs"
+  return (
+    <div
+      style={{
+        // backgroundImage: "url(/bg.png)",
+        height: "100vh",
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+      }}
     >
-      <form>
-        <Box
-          sx={{
-            marginTop: 8,
-            display: "flex",
-            flexDirection: "column",
-            alignItems: "center",
-					}}
-        ></Box>
-        <Box mb = {2}>
-          <TextField 
-            type = "number"
-            variant = "outlined"
-            label = "Ration Card Number"
-            fullWidth
-            autoFocus
-            {...register("rationCardNumber", { required: true, maxLength: 20 })}
-          />
-        </Box>
-        <Box mb = {2}>
-            <TextField
-            type = "number" 
-            variant = "outlined"
-            label = "Month"
-            min = "1"
-            max = "12"
-            fullWidth
-            autoFocus
-            {...register("month", { required: true, maxLength: 2 })}
-            />
-        </Box>
-        <Box mb = {2}>
-            <TextField
-            type = "number" 
-            variant = "outlined"
-            label = "Wheat"
-            fullWidth
-            autoFocus
-            {...register("wheat", { required: true, maxLength: 20 })}
-            />
-        </Box>
-        <Box mb = {2}>
-            <TextField 
-            type = "number"
-            variant = "outlined"
-            label = "Rice"
-            fullWidth
-            autoFocus
-            {...register("rice", { required: TextareaAutosize, maxLength: 20 })}
-            />
-        </Box>
-        <Box mb = {2}>
-            <TextField 
-            type = "number"
-            variant = "outlined"
-            label = "Kerosine"
-            fullWidth
-            autoFocus
-            {...register("kerosine", { required: true, maxLength: 20 })}
-            />
-        </Box>
-      </form>
-      <Button type = "submit" variant = "contained" color = "primary" onClick = {handleSubmit(onSubmit)} >Allocate</Button>
-    </Container>
+      <div
+        style={{
+          background: "white",
+          opacity: "90%",
+          margin: "auto",
+          width: "60%",
+          height: "600px",
+          padding: "5%",
+        }}
+      >
+        <h1>Customer Allocation</h1>
+        <Container maxWidth="xs">
+          <form>
+            <Box
+              sx={{
+                marginTop: 6,
+                display: "flex",
+                flexDirection: "column",
+                alignItems: "center",
+              }}
+            ></Box>
+            <Box mb={4}>
+              <TextField
+                type="number"
+                variant="outlined"
+                color="success"
+                label="Enter Ration Card No."
+                // rows={4}
+                // multiline
+                focused
+                fullWidth
+                autoFocus
+                {...register("rationCardNo", {
+                  required: true,
+                  maxLength: 20,
+                })}
+                // style={{ backgroundColor: "#E5E5E5" }}
+              />
+            </Box>
+            <Box mb={4}>
+              <TextField
+                type="number"
+                variant="outlined"
+                color="success"
+                label="Month"
+                // rows={4}
+                // multiline
+                focused
+                autoFocus
+                {...register("rationCardNo", {
+                  required: true,
+                  maxLength: 20,
+                })}
+                // style={{ backgroundColor: "#E5E5E5" }}
+              />
+            </Box>
+            <Box
+              mb={2}
+              style={{ display: "flex", justifyContent: "space-between" }}
+            >
+              <TextField
+                style={{ width: "100px" }}
+                type="number"
+                variant="outlined"
+                label="Wheat"
+                color="success"
+                InputProps={{
+                  endAdornment: (
+                    <InputAdornment position="end">kg</InputAdornment>
+                  ),
+                }}
+                focused
+                autoFocus
+                {...register("wheat", { required: true, maxLength: 4 })}
+              />
+              <TextField
+                type="number"
+                color="success"
+                variant="outlined"
+                label="Rice"
+                style={{ width: "100px" }}
+                focused
+                InputProps={{
+                  endAdornment: (
+                    <InputAdornment position="end">kg</InputAdornment>
+                  ),
+                }}
+                autoFocus
+                {...register("rice", {
+                  required: TextareaAutosize,
+                  maxLength: 4,
+                })}
+              />
+              <TextField
+                type="number"
+                variant="outlined"
+                color="success"
+                label="Kerosene"
+                style={{ width: "150px" }}
+                focused
+                InputProps={{
+                  endAdornment: (
+                    <InputAdornment position="end">litres</InputAdornment>
+                  ),
+                }}
+                autoFocus
+                {...register("kerosene", { required: true, maxLength: 3 })}
+              />
+            </Box>
+
+            <Button
+              type="submit"
+              variant="contained"
+              // color="primary"
+              style={{
+                background: "#17396B",
+                opacity: "100%",
+                margin: "50px 0",
+              }}
+              onClick={handleSubmit(onSubmit)}
+            >
+              Allocate Resources
+            </Button>
+          </form>
+        </Container>
+      </div>
     </div>
-    )
-}
+  );
+};
 
 export default IVAllocation;
