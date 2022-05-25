4dc4ef87bf23b6d4b9c323ca54dac86ed5371052
Neethu S <neethu2001nsr@gmail.com>
2022-05-26 00:28:50 +0530

    Added header

--- src/App.js
+++ src/App.js
@@ -1,28 +1,31 @@
-import React from 'react';
+import React from "react";
 import "./App.css";
-import { BrowserRouter,Route,Routes } from 'react-router-dom'
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import SignIn from "./SignIn";
-import RegisterPage from './Register'
-import IMAllocation from './IMAllocation'
-import SKAllocation from './SKAllocation'
-import BlockRegister from './BlockchainRegister'
+import RegisterPage from "./Register";
+import IMAllocation from "./IMAllocation";
+import SKAllocation from "./SKAllocation";
+import BlockRegister from "./BlockchainRegister";
+import Footer from "./components/Footer";
+// import Login from "./Login";
+import Header from "./components/Header";
 
 function App() {
-	return (
-		<div className="App">
-			<BrowserRouter>
-				<Routes>
-					<Route path = "/" element = {<SignIn/>}>
-					</Route>
-					<Route path = "/register" element = {<RegisterPage/>} /> 
-					<Route path = "/ivallocation" element = {<IMAllocation/>} /> 
-					<Route path = "/spallocation" element = {<SKAllocation/>} />
-					<Route path = "/blockregister" element = {<BlockRegister/>} />
-					</Routes>
-			</BrowserRouter>
-		</div>
-	);
+  return (
+    <div className="App">
+      <Header />
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<SignIn />}></Route>
+          <Route path="/register" element={<RegisterPage />} />
+          <Route path="/imallocation" element={<IMAllocation />} />
+          <Route path="/skallocation" element={<SKAllocation />} />
+          <Route path="/blockregister" element={<BlockRegister />} />
+        </Routes>
+      </BrowserRouter>
+      <Footer />
+    </div>
+  );
 }
 
 export default App;
-
