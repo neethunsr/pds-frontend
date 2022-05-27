e5d899894c0ab8c7efe59e14b66ecb43ddf3b9f4
Neethu S <neethu2001nsr@gmail.com>
2022-05-28 01:16:43 +0530

    Some changes to sign in and register pgs

--- src/Register.js
+++ src/Register.js
@@ -1,72 +1,93 @@
-import * as React from 'react';
-import { TextField,Button,Container,Box } from '@mui/material';
+import * as React from "react";
+import { TextField, Button, Container, Box, Avatar } from "@mui/material";
 import { useForm } from "react-hook-form";
+import LockOutlinedIcon from "@mui/icons-material/LockOutlined";
+import Typography from "@mui/material/Typography";
 
 const RegisterPage = () => {
-  const {register,handleSubmit} = useForm();
+  const { register, handleSubmit } = useForm();
 
   const onSubmit = (data) => {
-    console.log(data)
-    console.log("submission.....")
-  }
-
+    console.log(data);
+    console.log("submission.....");
+  };
 
   return (
-    <div className = "mt-5">
-      <h1>REGISTRATION</h1>
-    <Container 
-      maxWidth = "xs"
-    >
-      <form>
+    <div>
+      <Container maxWidth="xs">
         <Box
           sx={{
-						marginTop: 8,
-						display: "flex",
-						flexDirection: "column",
-						alignItems: "center",
-					}}
-        ></Box>
-        <Box mb = {2}>
-          <TextField 
-            variant = "outlined"
-            label = "user Id"
-            fullWidth
-            autoFocus
-            {...register("userId", { required: true, maxLength: 20 })}
-          />
+            marginTop: 8,
+            display: "flex",
+            flexDirection: "column",
+            alignItems: "center",
+            paddingBottom: "30px",
+          }}
+        >
+          <Avatar sx={{ m: 1, bgcolor: "#17396B" }}>
+            <LockOutlinedIcon />
+          </Avatar>
+          <Typography component="h1" variant="h5">
+            New User Registration
+          </Typography>
         </Box>
-        <Box mb = {2}>
-            <TextField 
-            variant = "outlined"
-            label = "Name"
-            fullWidth
-            autoFocus
-            {...register("name", { required: true, maxLength: 20 })}
+        <Box component="form" onSubmit={handleSubmit} noValidate sx={{ mt: 1 }}>
+          <Box mb={2}>
+            <TextField
+              variant="outlined"
+              label="user Id"
+              color="success"
+              fullWidth
+              autoFocus
+              {...register("userId", { required: true, maxLength: 20 })}
             />
-        </Box>
-        <Box mb = {2}>
-            <TextField 
-            variant = "outlined"
-            label = "MetaMask Address"
-            fullWidth
-            autoFocus
-            {...register("metaAddress", { required: true, maxLength: 20 })}
+          </Box>
+          <Box mb={2}>
+            <TextField
+              variant="outlined"
+              color="success"
+              label="Name"
+              fullWidth
+              autoFocus
+              {...register("name", { required: true, maxLength: 20 })}
             />
-        </Box>
-        <Box mb = {2}>
-            <TextField 
-            variant = "outlined"
-            label = "Phone Number"
-            fullWidth
-            autoFocus
-            {...register("phoneNumber", { required: true, maxLength: 20 })}
+          </Box>
+          <Box mb={2}>
+            <TextField
+              color="success"
+              variant="outlined"
+              label="MetaMask Address"
+              fullWidth
+              autoFocus
+              {...register("metaAddress", { required: true, maxLength: 20 })}
             />
+          </Box>
+          <Box>
+            <TextField
+              variant="outlined"
+              color="success"
+              label="Phone Number"
+              fullWidth
+              autoFocus
+              {...register("phoneNumber", { required: true, maxLength: 20 })}
+            />
+          </Box>
+          <Button
+            type="submit"
+            variant="contained"
+            color="primary"
+            onClick={handleSubmit(onSubmit)}
+            style={{
+              background: "#17396B",
+              margin: "40px 0",
+            }}
+          >
+            Submit
+          </Button>
         </Box>
-      </form>
-      <Button type = "submit" variant = "contained" color = "primary" onClick = {handleSubmit(onSubmit)} >Submit</Button>
-    </Container>
+      </Container>
     </div>
-  )
-}
+  );
+};
 
 export default RegisterPage;
