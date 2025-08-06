11627ef06564fa473211b45d78fb1051cb8c1d25
Akhilrs-RS <akhilrajanshiners@gmail.com>
2025-08-06 20:54:36 +0530

    update firebase creds

--- src/firebase.js
+++ src/firebase.js
@@ -11,19 +11,19 @@ import firebase from "firebase/compat/app";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 export const firebaseConfig = {
-	apiKey: "AIzaSyBkAQFqP5x6xEABYG-5Q1pB0Eb4EzGKp5o",
-	authDomain: "blockchain-pds-2025.firebaseapp.com",
-	projectId: "blockchain-pds-2025",
-	storageBucket: "blockchain-pds-2025.firebasestorage.app",
-	messagingSenderId: "1041268416766",
-	appId: "1:1041268416766:web:1f7bd54f5bcaaebb2985af",
-	measurementId: "G-8CC185HNNB",
+  apiKey: "AIzaSyCv0QURjsDdv1AdXNCfGBtwAMvsnuMp7Pk",
+  authDomain: "pds-using-blockchain.firebaseapp.com",
+  projectId: "pds-using-blockchain",
+  storageBucket: "pds-using-blockchain.firebasestorage.app",
+  messagingSenderId: "1046807604765",
+  appId: "1:1046807604765:web:7f8a20e09f71c86177e0f4",
+  measurementId: "G-6T4TV2SK0L",
 };
 
 // Initialize Firebase
 // const app = initializeApp(firebaseConfig);
 if (!firebase.apps.length) {
-	firebase.initializeApp(firebaseConfig);
+  firebase.initializeApp(firebaseConfig);
 }
 export let db = firebase.firestore();
 
